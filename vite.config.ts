5c1c5780e5cbcab0c249324e5895232af7c91e52
tomuraCreator <vladislavutkin92@gmail.com>
2023-02-20 17:10:46 +0400

    hotfix: fix root path to page resources

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  return {
-    build: {
-      assetsDir: 'phone-number-client',
-    },
+export default defineConfig({
+    base: "/phone-number-client/",
     plugins: [react()]
-  }
 })
